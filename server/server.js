dace81e3d892e37556ea8bfea6164be20ce7230a
HasanOmar1 <r3domar1@gmail.com>
2024-08-17 16:49:42 +0300

    updated origins

--- server/server.js
+++ server/server.js
@@ -10,7 +10,10 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://hasan-tolstoy-exam.netlify.app/",
+    ],
   })
 );
 app.use(express.json());
